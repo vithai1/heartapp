b510a6145a92ed991c3b25c2275586a4f74dfe0a
Vivi Thai <vithai1@berkeley.edu>
2023-12-20 18:21:14 -0800

    "finished"

---
+++ src/textbox.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './textbox.css';
+
+const TextBox = () => {
+  const [board, setBoard] = useState(Array(9).fill(''));
+
+  const handleBoxChange = (index, newText) => {
+    const newBoard = [...board];
+    newBoard[index] = newText;
+    setBoard(newBoard);
+
+    const editableElement = document.querySelector(`.box:nth-child(${index + 1})`);
+    if (editableElement) {
+      const range = document.createRange();
+      const selection = window.getSelection();
+      range.selectNodeContents(editableElement);
+      range.collapse(false);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  };
+
+  const renderBoxes = () => {
+    return board.map((text, index) => (
+      <div
+        key={index}
+        className="box"
+        contentEditable
+        onInput={(e) => handleBoxChange(index, e.currentTarget.textContent)}
+      >
+        {text}
+      </div>
+    ));
+  };
+
+  return <div className="board">{renderBoxes()}</div>;
+};
+
+export default TextBox;
